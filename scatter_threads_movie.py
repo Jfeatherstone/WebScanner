8d632ab20cfb38fa12393275648e224443443e65
Jack Featherstone <jack.featherstone@oist.jp>
2023-03-01 19:45:21 +0900

    Updated scatter threads movie script to have cli

--- scatter_threads_movie.py
+++ scatter_threads_movie.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import argparse
 import cv2
 import tqdm
 import os
@@ -16,7 +17,7 @@ def loadImages(path, n=None):
     elif os.path.isdir(path):
         images = []
         #images = [cv2.imread(path + '/' + f) for f in os.listdir(path)[:None] if f[-3:] in IMAGE_EXTENSIONS]
-        for f in np.sort(os.listdir(path)[:n]):
+        for f in tqdm.tqdm(np.sort(os.listdir(path)[:n])):
             if f[-3:].lower() in IMAGE_EXTENSIONS:
                 images.append(cv2.imread(path + '/' + f))
             
@@ -40,19 +41,15 @@ def getVideoFrames(videoPath):
     return frames
 
 
-def scatterThreads(inputPath, outputPath):
+def scatterThreads(inputPath, outputPath, regionOfInterest, threshold, dsFactor, fps, dtheta):
 
 
     videoFrames = loadImages(inputPath)
 
-    #regionOfInterest = [[60, 340], # y
-    #                    [350, 620]] # x
 
-    #videoFrames = [v[regionOfInterest[0][0]:regionOfInterest[0][1],regionOfInterest[1][0]:regionOfInterest[1][1]] for v in videoFrames]
+    videoFrames = [v[regionOfInterest[0][0]:regionOfInterest[0][1],regionOfInterest[1][0]:regionOfInterest[1][1]] for v in videoFrames]
     
     print(f'Loaded {len(videoFrames)} images.')
-
-    threshold = 30
 
     thresholdFrames = np.zeros((len(videoFrames), *videoFrames[0].shape[:2]))
 
@@ -73,11 +70,9 @@ def scatterThreads(inputPath, outputPath):
     
     images = []
 
-    fps = 20
     loop = 0
-    dsFactor = 8
 
-    for i in tqdm.tqdm(range(360), desc='Generating movie'):
+    for i in tqdm.tqdm(range(int(360 / dtheta)), desc='Generating movie'):
         fig = plt.figure(figsize=(7,7))
 
         ax = fig.add_subplot(projection='3d')
@@ -85,7 +80,7 @@ def scatterThreads(inputPath, outputPath):
         ax.scatter(scatterPoints[::dsFactor,2], scatterPoints[::dsFactor,1], scatterPoints[::dsFactor,0], s=.2)
         #ax.set_zlim([0, regionOfInterest[0][1] - regionOfInterest[0][0]])
         #ax.set_ylim([0, regionOfInterest[1][1] - regionOfInterest[1][0]])
-        ax.view_init(-160, i)
+        ax.view_init(-160, i*dtheta)
         
         fig.tight_layout()
         canvas = plt.get_current_fig_manager().canvas
@@ -99,9 +94,22 @@ def scatterThreads(inputPath, outputPath):
 
     images[0].save(outputPath, save_all=True, append_images=images[1:], duration=fps, loop=loop)
 
-if __name__ == '__main__':
 
-    inputPath = '/home/jack/Videos/Web Scanning/2023-02-27_Test_Laser3.MOV'
-    #inputPath = '/home/jack/Videos/Web Scanning/2023-02-27_Test_Burst_Laser2/'
-    outputPath = 'images/2023-02-27_Test_Burst_Laser2.gif'
-    scatterThreads(inputPath, outputPath)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(dest='inputPath', type=str)
+    parser.add_argument('-o', dest='outputPath', help='Output file path for the gif', default='output.gif')
+    parser.add_argument('-t', dest='threshold', type=float, help='Threshold pixel value to include in scatter', default=30)
+    parser.add_argument('--ds', dest='downsample', type=int, help='Factor to downsample scatter points by', default=10)
+    parser.add_argument('--fps', dest='fps', type=int, help='FPS of output gif', default=20)
+    parser.add_argument('--dt', dest='dtheta', type=float, help='Difference in angle between each subsequent view', default=1)
+
+    args = parser.parse_args()
+
+    regionOfInterest = [[1400, 3750], # y
+                        [2300, 5400]] # x
+
+    scatterThreads(args.inputPath, args.outputPath, regionOfInterest, args.threshold, args.downsample, args.fps, args.dtheta)
